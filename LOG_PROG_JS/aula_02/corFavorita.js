537d6c341aeb89fe6fe4e20665ae8b770db44a1a
Emille Nepomuceno <emillynepo638@yahoo.com>
2025-10-02 22:14:16 -0300

    nova atualização

---
+++ LOG_PROG_JS/aula_02/corFavorita.js
@@ -0,0 +1,7 @@
+// 1. Criando a variável com a cor favorita inicial
+let corFavorita = "preto";
+console.log("Cor favorita inicial:", corFavorita);
+
+// 2. Alterando o valor da variável
+corFavorita = "rosa";
+console.log("Nova cor favorita:", corFavorita);
