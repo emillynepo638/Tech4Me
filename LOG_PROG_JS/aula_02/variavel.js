85464e9e5dcd7e98eab4c2d9add0a505b09efaec
Emille Nepomuceno <emillynepo638@yahoo.com>
2025-10-02 22:11:53 -0300

    atualização

---
+++ LOG_PROG_JS/aula_02/variavel.js
@@ -0,0 +1,11 @@
+// 1. Declarando a variável (sem valor)
+let idade;
+console.log(idade); 
+
+// 2. Atribuindo um valor
+idade = 15;
+console.log(idade); 
+
+// 3. Alterando o valor
+idade = 20;
+console.log(idade);
